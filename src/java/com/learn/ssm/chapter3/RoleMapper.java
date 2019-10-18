2bf5d74cfe8b831804e9342995e4af7277c65fa6
chao.qian <chao.qian@tinno.com>
2019-10-18 16:37:38 +0800

    first commit

---
+++ src/java/com/learn/ssm/chapter3/RoleMapper.java
@@ -0,0 +1,13 @@
+package java.com.learn.ssm.chapter3;
+
+/**
+ * @ProjectName: learnSSM
+ * @Package: java.com.learn.ssm.chapter3
+ * @ClassName: RoleMapper
+ * @Author: android2
+ * @Description:
+ * @Date: 2019/10/14 下午2:37
+ * @Version: 1.0
+ */
+public interface RoleMapper {
+}
